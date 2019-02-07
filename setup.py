122f7fa3c7f9dae1fcf4b0b5cc1cab007098ebf0
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-02-07 12:32:10 +0100

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE.md") as f:
+    license = f.read()
+
+setup(
+    name="UniDAQ",
+    version="0.9.2",
+    description="A framework for automated DAQ for semiconductor device testing",
+    long_description=readme,
+    author="Dominic Blöch",
+    author_email="rhtbapat@gmail.com",
+    url="https://github.com/Chilldose/UniDAQ",
+    license=license,
+    packages=find_packages()
+)
