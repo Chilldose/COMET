1dfe6fdfddf91711223e4e92845054d307bc45e2
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2019-11-12 12:35:05 +0100

    Single Measurement design WIP

---
+++ COMET/ui_plugins/MeasurementDetails.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from PyQt5.QtWidgets import *
+from .. import engineering_notation as en
+import time
+
+from ..utilities import ramp_voltage_job, transformation, change_axis_ticks
+
+class MeasurementDetails_window:
+
+    def __init__(self, GUI, layout):
+
+        self.variables = GUI
+        self.layout = layout
+
+        # Settings tab
+        single_widget = QWidget()
+        self.single_strip = self.variables.load_QtUi_file("singlemeasurement.ui", single_widget)
+        self.layout.addWidget(single_widget)
