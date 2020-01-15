b33f705180e88319467dd3a6e13f68e99e5f09c0
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2020-01-15 10:55:42 +0100

    new data vis option and fixed error where plot option did not show every time

--- COMET/ui_plugins/DataVisualization_window.py
+++ COMET/ui_plugins/DataVisualization_window.py
@@ -261,6 +261,7 @@ class DataVisualization_window:
                                     # If this entry is missing generate an empty dict so options can be added later on
                                     self.selected_plot_option = (ana, plot_name, "{}".format(plotLabel[0].strip()))
                                     plt_opt["{}".format(plotLabel[0].strip())] = {}
+                                    self.update_plot_options_tree(plot)
                                     return
                         except Exception as err:
                             self.selected_plot_option = ()
