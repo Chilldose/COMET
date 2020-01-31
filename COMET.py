ad1f5939a608fc2f771ec1a87bd24eb01ab5992e
Dominic Blöch <41591574+Chilldose@users.noreply.github.com>
2020-01-31 12:06:20 +0100

    Update COMET.py

--- COMET.py
+++ COMET.py
@@ -2,4 +2,11 @@ if __name__ == '__main__':
     import os
     os.system("set QT_SCALE_FACTOR=1.5")
     from COMET.main import main# This starts the actual measurement software
-    main()
+    try:
+        main()
+    except:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.system("cd {}".format(dir_path))
+        # For Linux on remote Displays
+        os.system("export DISPLAY:=0.0")
+        main()
