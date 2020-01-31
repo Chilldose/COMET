da6733d5c992963a9e02875c2a0b49ed28a09a4e
Dominic Blöch <41591574+Chilldose@users.noreply.github.com>
2020-01-31 12:13:03 +0100

    fix path error

--- COMET.py
+++ COMET.py
@@ -1,12 +1,11 @@
 if __name__ == '__main__':
     import os
+    import platform
+    OS = platform.system()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.system("cd {}".format(dir_path))
     os.system("set QT_SCALE_FACTOR=1.5")
-    from COMET.main import main# This starts the actual measurement software
-    try:
-        main()
-    except:
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        os.system("cd {}".format(dir_path))
-        # For Linux on remote Displays
+    if OS == "Linux":
         os.system("export DISPLAY:=0.0")
-        main()
+    from COMET.main import main # This starts the actual measurement software
+    main()
