a414f35a021a01937b90a1e99e69b4244b0f7b4a
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2020-04-16 13:33:34 +0200

    pip req update

--- environment_setup.py
+++ environment_setup.py
@@ -14,18 +14,27 @@ class environement_setup:
         print(python_type)
         print(python_version)
 
+        if python_version[0] != 3:
+            print("Warning: Python version must be 3.7.x to work properly. Errors may rise!")
+
         try:
             requirement_file = sys.argv[1]
-        except: requirement_file = "requirements.yml"
-
-        try:
-            cmd = "conda env create -f {}".format(requirement_file)
-            os.system(cmd)
         except:
-            print("No Anaconda installation found please install all modules listed in the requirements.yml via pip")
+            print('You did not pass a conda environment file. Do you want to install the non conda environment? [Y/n]?')
+            x = input()
+            if x.upper()=="Y" or x.upper()=="Yes":
+                requirement_file = "requirements_pip.yml"
+                # Install pip env
+                os.system("pip install -r {}".format(requirement_file))
+                sys.exit(0)
+            else:
+                print("Aborting...")
+                sys.exit(0)
 
-        if python_version[0] != 3:
-            print("Warning: Python version must be 3.x to work properly")
+        # Install anaconda env
+        cmd = "conda env create -f {}".format(requirement_file)
+        os.system(cmd)
+        sys.exit(0)
 
 if __name__ == "__main__":
     env = environement_setup()
