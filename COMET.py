8a001fa7c34c0f85c3e73cfc292d1653517d62aa
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2020-05-07 12:51:05 +0200

    Fixed rare bug on linux machines where the cwd can be wrong

--- COMET.py
+++ COMET.py
@@ -7,6 +7,10 @@ if __name__ == '__main__':
     os.system("set QT_SCALE_FACTOR=1.5")
 
     if OS == "Linux":
-        os.system("export DISPLAY:=0.0")
+        try:
+            os.chdir(os.path.normpath(os.path.dirname(__file__)))
+            os.system("export DISPLAY:=0.0")
+        except:
+            pass
     from COMET.main import main # This starts the actual measurement software
     main()
