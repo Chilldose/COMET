f1175c2edc1cbebb0873e76c8b3f74d8ec6e0ee5
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2020-10-07 11:02:26 +0200

    Update upload_to_db.py

--- COMET/ui_plugins/upload_to_db.py
+++ COMET/ui_plugins/upload_to_db.py
@@ -2,6 +2,9 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QDialog
 from ..measurement_plugins.forge_tools import tools
 from time import sleep
 from PyQt5.QtWidgets import QApplication, QFileDialog
+import subprocess, os
+import logging
+import glob
 
 
 class CMS_DB_loader(QDialog):
@@ -10,6 +13,7 @@ class CMS_DB_loader(QDialog):
         super(CMS_DB_loader, self).__init__(None)
         self.gui = gui
         self.tools = tools
+        self.log = logging.getLogger(__name__)
 
         layout = QVBoxLayout(self)
 
@@ -31,11 +35,25 @@ class CMS_DB_loader(QDialog):
     def upload_action(self):
         """Starts the script to upload data to DB"""
         self.DBwidget.Upload_Button.setEnabled(False)
-        for i in range(10):
-            self.DBwidget.progressBar.setValue(((i+1)/10.)*100)
-            self.DBwidget.which_file.setText("Uploading file: {}".format(i))
-            sleep(0.2)
-            QApplication.processEvents()
+        directory = self.DBwidget.directory_edit.text()
+        if os.path.isdir(directory):
+            file_list = glob.glob(directory + "/*.xml")
+            if "CMSxmlTemplate" in self.gui.variables.default_values_dict:
+                uploader = self.gui.variables.default_values_dict["CMSxmlTemplate"].get("DB_uploader_API_module", "")
+                for i, file in enumerate(file_list):
+                    self.DBwidget.which_file.setText("Uploading file: {}".format(os.path.basename(file)))
+                    try:
+                        proc = subprocess.run("python " + os.path.normpath(uploader) + " " + os.path.normpath(file), capture_output=True)
+                        answer = proc.stdout.decode()
+                        self.log.info("Uploader return: {}".format(answer))
+                    except:
+                        self.log.error("An error happened while uploading file {}".format(file), exc_info=True)
+                    # todo: username and pw query
+                    self.DBwidget.progressBar.setValue(((i+1)/len(file_list))*100)
+                    QApplication.processEvents()
+                    sleep(0.3)
+        else:
+            self.log.error("The path {} does not exist!".format(directory))
         self.DBwidget.Upload_Button.setEnabled(True)
         self.DBwidget.which_file.setText("")  
         self.DBwidget.progressBar.setValue(0.)
